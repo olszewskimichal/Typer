5a8ce912e8bf4ffb5965425e65e2405d7a7d899f
Michał <olszewskimichal@outlook.com>
2018-07-16 18:17:07 +0200

    inicjalizacja projektu

---
+++ src/main/java/pl/michal/olszewski/typer/TyperApplication.java
@@ -0,0 +1,12 @@
+package pl.michal.olszewski.typer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TyperApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TyperApplication.class, args);
+  }
+}
