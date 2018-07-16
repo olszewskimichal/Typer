f1b1382756428a3ef9ea4dc2786016f7fca9bd69
Michał <olszewskimichal@outlook.com>
2018-07-16 23:16:08 +0200

    add final keyword

--- src/main/java/pl/michal/olszewski/typer/policy/DadBetPolicy.java
+++ src/main/java/pl/michal/olszewski/typer/policy/DadBetPolicy.java
@@ -5,8 +5,8 @@ import pl.michal.olszewski.typer.bet.CheckBetMatchEvent;
 
 public class DadBetPolicy implements BetPolicy {
 
-  static long POINTS_FOR_CORRECT_RESULT = 1L;
-  static long POINTS_FOR_INCORRECT_RESULT = 0L;
+  static final long POINTS_FOR_CORRECT_RESULT = 1L;
+  static final long POINTS_FOR_INCORRECT_RESULT = 0L;
 
   @Override
   public BetChecked applyPolicy(CheckBetMatchEvent checkBetMatchEvent) {
