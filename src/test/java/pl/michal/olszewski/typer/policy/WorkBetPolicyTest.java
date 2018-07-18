9da86080e4f9a311caff21eda1a149008e5b0874
Michał <olszewskimichal@outlook.com>
2018-07-18 21:43:29 +0200

    missing tests

--- src/test/java/pl/michal/olszewski/typer/policy/WorkBetPolicyTest.java
+++ src/test/java/pl/michal/olszewski/typer/policy/WorkBetPolicyTest.java
@@ -21,6 +21,7 @@ class WorkBetPolicyTest {
       "1,2,0,2,2",
       "2,1,3,0,1",
       "2,1,1,1,0",
+      "1,2,1,1,0",
       "2,1,2,3,0"
   })
   void shouldCalculatePoints(Long betAwayGoals, Long betHomeGoals, Long expectedAwayGoals, Long expectedHomeGoals, Long points) {
