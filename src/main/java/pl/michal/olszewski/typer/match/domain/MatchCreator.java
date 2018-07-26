a319d9ccda18034c2505132c349c66396067ff9b
Michał <olszewskimichal@outlook.com>
2018-07-26 18:44:19 +0200

    TDD - phase refactor - creating new matches

--- src/main/java/pl/michal/olszewski/typer/match/domain/MatchCreator.java
+++ src/main/java/pl/michal/olszewski/typer/match/domain/MatchCreator.java
@@ -2,18 +2,13 @@ package pl.michal.olszewski.typer.match.domain;
 
 import java.util.Objects;
 import pl.michal.olszewski.typer.match.dto.CreateNewMatch;
-import pl.michal.olszewski.typer.match.dto.IllegalMatchMemberException;
 
 class MatchCreator {
 
   Match from(CreateNewMatch createNewMatch) {
     Objects.requireNonNull(createNewMatch);
-    if (createNewMatch.getAwayTeamId() == null || createNewMatch.getHomeTeamId() == null) {
-      throw new IllegalMatchMemberException("One of team not exists");
-    }
-    if (createNewMatch.getHomeTeamId().equals(createNewMatch.getAwayTeamId())) {
-      throw new IllegalMatchMemberException("You cannot create match with two the same teams");
-    }
+    createNewMatch.validCommand();
+
     return Match.builder()
         .homeTeamId(createNewMatch.getHomeTeamId())
         .awayTeamId(createNewMatch.getAwayTeamId())
