03314ab33e519ed65edf346ed0a827c90a7b24ba
Michał <olszewskimichal@outlook.com>
2018-07-27 17:23:48 +0200

    TDD - phase refactor - creating new predictions

--- src/main/java/pl/michal/olszewski/typer/bet/BetCreator.java
+++ src/main/java/pl/michal/olszewski/typer/bet/BetCreator.java
@@ -1,19 +1,12 @@
 package pl.michal.olszewski.typer.bet;
 
 import java.util.Objects;
-import pl.michal.olszewski.typer.bet.dto.IllegalGoalArgumentException;
-import pl.michal.olszewski.typer.match.dto.MatchNotFoundException;
 
 class BetCreator {
 
   Bet from(CreateNewBet createNewBet) {
     Objects.requireNonNull(createNewBet);
-    if (createNewBet.getBetAwayGoals() == null || createNewBet.getBetHomeGoals() == null) {
-      throw new IllegalGoalArgumentException("Nieprawidłowa ilość bramek");
-    }
-    if (createNewBet.getMatchId() == null) {
-      throw new MatchNotFoundException("Nie podano jaki mecz zostal obstawiony");
-    }
+    createNewBet.validCommand();
     return Bet.builder()
         .betAwayGoals(createNewBet.getBetAwayGoals())
         .betHomeGoals(createNewBet.getBetHomeGoals())
