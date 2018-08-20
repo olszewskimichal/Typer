2838199ba425a2c0265499b9ac84fcce13957c48
olszewskimichal <olszewskimichal@outlook.com>
2018-08-20 19:02:33 +0200

    extract homeGoals and awayGoals from score

--- src/main/java/pl/michal/olszewski/typer/livescore/MatchDTO.java
+++ src/main/java/pl/michal/olszewski/typer/livescore/MatchDTO.java
@@ -1,5 +1,7 @@
 package pl.michal.olszewski.typer.livescore;
 
+import static java.lang.Long.valueOf;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.time.LocalDate;
 import lombok.Value;
@@ -17,4 +19,12 @@ class MatchDTO {
   private String status;
   @JsonProperty("league_id")
   private Long leagueId;
+
+  public Long getHomeGoals() {
+    return valueOf(getScore().split("-")[0].trim());
+  }
+
+  public Long getAwayGoals() {
+    return valueOf(getScore().split("-")[1].trim());
+  }
 }
