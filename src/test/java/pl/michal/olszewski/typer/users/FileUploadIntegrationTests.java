b5012295b89e65c575c07189268c4888df2f6b4e
olszewskimichal <olszewskimichal@outlook.com>
2018-09-20 19:52:09 +0200

    testy dla obsługi plików

---
+++ src/test/java/pl/michal/olszewski/typer/users/FileUploadIntegrationTests.java
@@ -0,0 +1,50 @@
+package pl.michal.olszewski.typer.users;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.multipart.MultipartFile;
+import pl.michal.olszewski.typer.file.FileStorageService;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Matchers.any;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class FileUploadIntegrationTests {
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@MockBean
+	private FileStorageService storageService;
+
+	@LocalServerPort
+	private int port;
+
+	@Test
+	public void shouldUploadFile() throws Exception {
+		ClassPathResource resource = new ClassPathResource("testupload.txt", getClass());
+
+		MultiValueMap<String, Object> map = new LinkedMultiValueMap<String, Object>();
+		map.add("file", resource);
+		ResponseEntity<String> response = this.restTemplate.postForEntity("/uploadFile", map,
+				String.class);
+
+		assertThat(response.getStatusCode()).isEqualByComparingTo(HttpStatus.OK);
+		then(storageService).should().storeFile(any(MultipartFile.class));
+	}
+}
---
+++ src/test/java/pl/michal/olszewski/typer/users/FileUploadTests.java
@@ -0,0 +1,39 @@
+package pl.michal.olszewski.typer.users;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import pl.michal.olszewski.typer.file.FileStorageService;
+
+import static org.mockito.BDDMockito.then;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.fileUpload;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
+@SpringBootTest
+public class FileUploadTests {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private FileStorageService storageService;
+
+    @Test
+    public void shouldSaveUploadedFile() throws Exception {
+        MockMultipartFile multipartFile = new MockMultipartFile("file", "test.txt",
+                "text/plain", "Spring Framework".getBytes());
+        this.mvc.perform(fileUpload("/uploadFile").file(multipartFile))
+                .andExpect(status().isOk());
+        then(this.storageService).should().storeFile(multipartFile);
+    }
+
+
+}
