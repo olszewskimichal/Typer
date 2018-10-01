b332a65c0ed0bc4f069b48e6810846eef213e977
olszewskimichal <olszewskimichal@outlook.com>
2018-10-01 21:57:35 +0200

    Creating Bet from REST

--- src/main/java/pl/michal/olszewski/typer/bet/domain/BetRestController.java
+++ src/main/java/pl/michal/olszewski/typer/bet/domain/BetRestController.java
@@ -1,23 +1,41 @@
 package pl.michal.olszewski.typer.bet.domain;
 
+import javax.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.michal.olszewski.typer.bet.dto.command.CreateNewBet;
 import pl.michal.olszewski.typer.bet.dto.read.BetInfo;
 
 @RestController
 @RequestMapping("/api/bet")
 public class BetRestController {
-    private final BetFinder betFinder;
 
-    public BetRestController(BetFinder betFinder) {
-        this.betFinder = betFinder;
-    }
+  private final BetFinder betFinder;
+  private final BetSaver betSaver;
 
-    @GetMapping("/{id}")
-    ResponseEntity<BetInfo> getBetInfoById(@PathVariable("id") Long id) {
-        return ResponseEntity.ok(betFinder.findOneOrThrow(id).toBetInfo());
-    }
+  public BetRestController(BetFinder betFinder, BetSaver betSaver) {
+    this.betFinder = betFinder;
+    this.betSaver = betSaver;
+  }
+
+  @GetMapping("/{id}")
+  ResponseEntity<BetInfo> getBetInfoById(@PathVariable("id") Long id) {
+    return ResponseEntity.ok(betFinder.findOneOrThrow(id).toBetInfo());
+  }
+
+
+  @PostMapping("")
+  ResponseEntity<String> createNewBet(@Valid @ModelAttribute("createNewBet") CreateNewBet createNewBet/*, Errors errors*/) {
+    /*if (errors.hasErrors()) {
+      return ResponseEntity.badRequest().body(errors.getAllErrors().toString());
+    }*/
+    Bet from = BetCreator.from(createNewBet);
+    betSaver.save(from);
+    return ResponseEntity.ok("OK");
+  }
 }
--- src/test/java/pl/michal/olszewski/typer/bet/domain/BetRestControllerIntegrationTest.java
+++ src/test/java/pl/michal/olszewski/typer/bet/domain/BetRestControllerIntegrationTest.java
@@ -1,5 +1,14 @@
 package pl.michal.olszewski.typer.bet.domain;
 
+import static org.hamcrest.Matchers.is;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,52 +20,68 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import static org.hamcrest.Matchers.is;
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 class BetRestControllerIntegrationTest {
 
-    @Autowired
-    private WebApplicationContext wac;
-    @Autowired
-    private BetSaver betSaver;
+  @Autowired
+  private WebApplicationContext wac;
+  @Autowired
+  private BetSaver betSaver;
 
-    private MockMvc mvc;
+  private MockMvc mvc;
 
-    @BeforeEach
-    void setUp() {
-        betSaver.deleteAll();
-        mvc = MockMvcBuilders.webAppContextSetup(wac).build();
-    }
+  @BeforeEach
+  void setUp() {
+    betSaver.deleteAll();
+    mvc = MockMvcBuilders.webAppContextSetup(wac).build();
+  }
 
-    @Test
-    void shouldReturnNotFoundWhenBetByIdNotExist() throws Exception {
-        mvc.perform(get("/api/bet/254"))
-                .andDo(print())
-                .andExpect(status().isNotFound());
-    }
+  @Test
+  void shouldReturnNotFoundWhenBetByIdNotExist() throws Exception {
+    mvc.perform(get("/api/bet/254"))
+        .andDo(print())
+        .andExpect(status().isNotFound());
+  }
 
-    @Test
-    void shouldReturnBetInfoWhenBetByIdExists() throws Exception {
-        Bet bet = Bet.builder().userId(1L).matchRoundId(2L).matchId(3L).betHomeGoals(4L).betAwayGoals(5L).points(6L).status(BetStatus.CHECKED).build();
-        betSaver.save(bet);
-        mvc.perform(get("/api/bet/" + bet.getId()))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
-                .andExpect(jsonPath("$.id", is(bet.getId().intValue())))
-                .andExpect(jsonPath("$.userId", is(1)))
-                .andExpect(jsonPath("$.roundId", is(2)))
-                .andExpect(jsonPath("$.matchId", is(3)))
-                .andExpect(jsonPath("$.homeGoals", is(4)))
-                .andExpect(jsonPath("$.awayGoals", is(5)))
-                .andExpect(jsonPath("$.points", is(6)))
-                .andExpect(jsonPath("$.status", is("CHECKED")));
+  @Test
+  void shouldReturnBetInfoWhenBetByIdExists() throws Exception {
+    Bet bet = Bet.builder().userId(1L).matchRoundId(2L).matchId(3L).betHomeGoals(4L).betAwayGoals(5L).points(6L).status(BetStatus.CHECKED).build();
+    betSaver.save(bet);
+    mvc.perform(get("/api/bet/" + bet.getId()))
+        .andExpect(status().isOk())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+        .andExpect(jsonPath("$.id", is(bet.getId().intValue())))
+        .andExpect(jsonPath("$.userId", is(1)))
+        .andExpect(jsonPath("$.roundId", is(2)))
+        .andExpect(jsonPath("$.matchId", is(3)))
+        .andExpect(jsonPath("$.homeGoals", is(4)))
+        .andExpect(jsonPath("$.awayGoals", is(5)))
+        .andExpect(jsonPath("$.points", is(6)))
+        .andExpect(jsonPath("$.status", is("CHECKED")));
 
-    }
+  }
+
+  @Test
+  void shouldCreateNewBet() throws Exception {
+    mvc.perform(post("/api/bet/")
+        .param("matchId", "1")
+        .param("userId", "1")
+        .param("matchRoundId", "1")
+        .param("betAwayGoals", "1")
+        .param("betHomeGoals", "1"))
+        .andExpect(status().isOk());
+  }
+
+  @Test
+  void shouldThrowExceptionWhenMatchIdIsNullOnCreateNewBet() throws Exception { //TODO zrobić podobne testy dla innych wartosci/walidacji
+    mvc.perform(post("/api/bet/")
+        .param("userId", "1")
+        .param("matchRoundId", "1")
+        .param("betAwayGoals", "1")
+        .param("betHomeGoals", "1"))
+        .andDo(print())
+        .andExpect(status().isBadRequest());
+  }
 
 }
